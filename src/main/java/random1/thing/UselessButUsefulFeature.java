42adfc10119d5e1a425262b911ec05133a3218a6
Ben Ryan <lemastersoda@gmail.com>
2018-10-01 15:47:18 -0500

    Added 1.13.1 compatibility

--- src/main/java/random1/thing/UselessButUsefulFeature.java
+++ src/main/java/random1/thing/UselessButUsefulFeature.java
@@ -6,13 +6,14 @@ import org.bukkit.Bukkit;
 
 /**
  * Copyright (c) Redraskal 2018.
+ * [!] This works on Spigot 1.8.8 & 1.13.1
  */
 public class UselessButUsefulFeature {
 
   /**
    * Ever wanted to literally change the max player count on a Spigot server?
    * Ever realized you actually can't do that because Minecraft is stupid?
-   * WELL NOW YOU CAN (If you are using 1.8.8 Spigot because I'm lazy and won't make this work on every MC version)
+   * WELL NOW YOU CAN
    * @param maxPlayers  the new player count
    * @throws ReflectiveOperationException
    */
@@ -21,12 +22,16 @@ public class UselessButUsefulFeature {
     Object nmsDedicatedPlayerList = getCraftbukkitClass("CraftServer").getMethod("getHandle").invoke(craftServer);
     Object nmsDedicatedServer = getNMSClass("DedicatedPlayerList").getMethod("getServer").invoke(nmsDedicatedPlayerList);
 
-    Field field_v = getNMSClass("MinecraftServer").getDeclaredField("v");
+    Field field_v = getNMSClass("MinecraftServer").getDeclaredField(getNMSVersion().equals("v1_13_R2") ? "s" : "v");
     field_v.setAccessible(true);
     Object nmsPlayerList = field_v.get(nmsDedicatedServer);
     Field field_maxPlayers = getNMSClass("PlayerList").getDeclaredField("maxPlayers");
     field_maxPlayers.setAccessible(true);
     field_maxPlayers.set(nmsPlayerList, maxPlayers);
+  }
+
+  public static String getNMSVersion() {
+    return Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
   }
 
   public static Class<?> getNMSClass(String name) {
