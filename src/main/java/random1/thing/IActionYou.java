d22db800562a7a11c930befe548e074f5fe75386
wherkamp <wherkamp@kingtux.me>
2018-10-31 11:46:12 -0400

    Streams are cool @supertassu

--- src/main/java/random1/thing/IActionYou.java
+++ src/main/java/random1/thing/IActionYou.java
@@ -4,17 +4,23 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.util.stream.Stream;
+
 import static org.bukkit.ChatColor.RED;
 
 public class IActionYou {
 
     // This is a very complex method, which invokes another function to get the message to send to the player.
-    public static void doAction(String action, Player giver, Player target){
+    public static void doAction(String action, Player giver, Player target) {
         Bukkit.broadcastMessage(ChatColor.GREEN + giver.getName() + " " + action + " " + target.getName());
     }
 
     public static void doAnotherAction(String... actions) {
-        Bukkit.broadcastMessage(RED + actions[0]);
+        Stream.of(actions).forEach(IActionYou::printRed);
+    }
+
+    public static void printRed(String what) {
+        Bukkit.broadcastMessage(RED + what);
     }
 
 }
